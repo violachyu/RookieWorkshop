03af9b0ac6d72220e9b5bdb5ff7c130cf3c22516
violachyu <violachyu@nine-yi.com>
2021-05-30 20:57:25 +0800

    Case001 - Books WebAPI with Repo, Models

---
+++ Case/Viola/Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RookieWorkshop.Models;
+using RookieWorkshop.Repositories;
+
+namespace RookieWorkshop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBooks(int id)
+        {
+            return await _bookRepository.Get(id);
+        }
+
+    }
+}
